d3a124f742e51bb85e361092b0f58a67a0d06b41
leonid <e_leokim@yahoo.com>
2020-05-15 23:21:52 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
