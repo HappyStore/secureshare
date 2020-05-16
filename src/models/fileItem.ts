33eff4d360f58192637721a7a081436847f1bd46
leonid <e_leokim@yahoo.com>
2020-05-16 12:40:34 -0700

    add basic store functionality

--- src/models/fileItem.ts
+++ src/models/fileItem.ts
@@ -3,5 +3,16 @@ export interface FileItemModel {
     Extension: string;
     Path?: string;
     Link?: string;
-    Status: 'loading' | 'ready'
+    Status: 'loading' | 'ready';
+}
+
+export function ValidateFileItem(fileItem: FileItemModel): string | null {
+    if (!fileItem.Name || fileItem.Name.length === 0) {
+        return 'Invalid file name';
+    }
+    if (!fileItem.Extension || fileItem.Extension.length === 0) {
+        return 'Invalid file extension';
+    }
+
+    return null;
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,72 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { FileItemModel, ValidateFileItem } from '@/models/fileItem';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
+interface ApplicationState {
+  logs: string[];
+  userFiles: FileItemModel[];
+  downloadFiles: FileItemModel[];
+}
+
+export default new Vuex.Store<ApplicationState>({
   state: {
+    logs: [],
+    userFiles: [],
+    downloadFiles: []
   },
   mutations: {
+    PUSH_LOG(state, strToAppend: string) {
+      state.logs.push(strToAppend);
+    },
+    PUSH_USER_FILE(state, fileItem: FileItemModel) {
+      state.userFiles.push(fileItem);
+    },
+    UPDATE_USER_FILE(state, fileItem: FileItemModel) {
+      // TODO: UPDATE_USER_FILE
+    },
+    PUSH_DOWNLOAD_FILE(state, fileItem: FileItemModel) {
+      state.downloadFiles.push(fileItem);
+    },
+    UPDATE_DOWNLOAD_FILE(state, fileItem: FileItemModel) {
+      // TODO: UPDATE_DOWNLOAD_FILE
+    }
   },
   actions: {
+    appendLog({ commit }, strToAppend: string) {
+      if (!strToAppend || strToAppend.length === 0) {
+        return;
+      }
+      commit('PUSH_LOG', strToAppend);
+    },
+    addNewUserFile({ commit }, fileItem: FileItemModel) {
+      const errMsg = ValidateFileItem(fileItem);
+      if (errMsg) {
+        throw new Error(errMsg);
+      }
+      commit('PUSH_USER_FILE', fileItem);
+    },
+    updateUserFile({ commit }, fileItem: FileItemModel) {
+      const errMsg = ValidateFileItem(fileItem);
+      if (errMsg) {
+        throw new Error(errMsg);
+      }
+      commit('UPDATE_USER_FILE', fileItem);
+    },
+    addDownloadFile({ commit }, fileItem: FileItemModel) {
+      const errMsg = ValidateFileItem(fileItem);
+      if (errMsg) {
+        throw new Error(errMsg);
+      }
+      commit('PUSH_DOWNLOAD_FILE', fileItem);
+    },
+    updateDownloadFile({ commit }, fileItem: FileItemModel) {
+      const errMsg = ValidateFileItem(fileItem);
+      if (errMsg) {
+        throw new Error(errMsg);
+      }
+      commit('UPDATE_DOWNLOAD_FILE', fileItem);
+    }
   },
-  modules: {
-  }
 })
